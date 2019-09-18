21d5b799e5995db506e91b7b713c2a5a9bd337f9
joldiazch <jluis.diaz@udea.edu.co>
2019-09-18 21:35:59 +0000

    modificated file called 4-*

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <string.h>
  
 int main()
 {
-	char string[40];
-	strcpy(string, "\"Programming is like building a multilingual puzzle");
-	puts(string);
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
