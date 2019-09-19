5d4f407c2eb8de75b927dd3ae8869c67af482643
joldiazch <jluis.diaz@udea.edu.co>
2019-09-19 22:09:52 +0000

    add 3-*.c file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,21 @@
 int main(void)
 {
 	char c;
+	char b;
 
 	c = 'a';
+
+	b = 'a';
 
 	while (c <= 'z')
 	{
 		putchar(c);
 		c = c + 1;
+	}
+	while (b <= 'z')
+	{
+		putchar(toupper(b));
+		b = b + 1;
 	}
 	putchar('\n');
 	return (0);
