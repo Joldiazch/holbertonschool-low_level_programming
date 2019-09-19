9f2f7b945a8a31e6d147489ee649d0044c3db33b
joldiazch <jluis.diaz@udea.edu.co>
2019-09-19 21:41:10 +0000

    add 2-*.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point print
+*
+* Return: Always 0 (sucess)
+**/
+int main(void)
+{
+	char c;
+	c = 'a';
+	while(c <= 'z')
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+}
