03cd4b58453e9e139893b425ebc8107ec03e3282
joldiazch <jluis.diaz@udea.edu.co>
2019-09-20 02:27:12 +0000

    add 6-*.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry point print
+*
+* Return: Always 0 (sucess)
+**/
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/tets.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry point print
+*
+* Return: Always 0 (sucess)
+**/
+int main(void)
+{
+	putchar(48);
+	putchar('\n');
+	return (0);
+}
