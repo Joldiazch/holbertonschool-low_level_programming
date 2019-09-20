df568e7127b2d08ceb5a5784cb6ae89a5df5e3d5
joldiazch <jluis.diaz@udea.edu.co>
2019-09-20 00:06:31 +0000

    add 5-*.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry point print
+*
+* Return: Always 0 (sucess)
+**/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+	putchar('\n');
+	return (0);
+}
