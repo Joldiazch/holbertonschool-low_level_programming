bca0647fd32241f968ccd756e93017b156b916f6
joldiazch <jluis.diaz@udea.edu.co>
2019-09-25 21:55:06 +0000

    modificated 3-*.c file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* _islower: check if C character is a lower .
+* _islower - check if C character is a lower .
 *
 * @c: this is a character.
 * Return: 1 if character is a lower or 0 in other case.
