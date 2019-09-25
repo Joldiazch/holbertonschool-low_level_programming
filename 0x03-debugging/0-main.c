9eebe75217edf075124cdee4cc5a9887ad951bcd
joldiazch <jluis.diaz@udea.edu.co>
2019-09-25 03:32:10 +0000

    modificated 0-*.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 /**
-* main - Entry point print
+* main - Determina si i es positivo negativo o cero
 *
 * Return: Always 0 (sucess)
 **/
 int main(void)
 {
-	print_alphabet();
+	int i;
+
+	i=0;
+	positive_or_negative(i);
+
 	return (0);
 }
