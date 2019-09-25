764b93ad035d0d2df894f72b1c126d31949bc076
joldiazch <jluis.diaz@udea.edu.co>
2019-09-25 20:40:23 +0000

    add 2- files

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* main - Entry point print
+*
+* Return: Always 0 (sucess)
+**/
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_alphabet_x10 - print de alphabet for 10
+*
+*
+* Return: void
+*
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
