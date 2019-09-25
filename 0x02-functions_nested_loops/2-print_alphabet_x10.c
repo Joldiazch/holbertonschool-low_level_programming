8360211d2c4c838930f410f315fef86d16b42d0e
joldiazch <jluis.diaz@udea.edu.co>
2019-09-25 20:55:52 +0000

    modificated 2-p*.c and yout main.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,16 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char c = 'a';
 
 	for (i = 0; i <= 10; i++)
 	{
-		print_alphabet();
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		c = 'a';
 	}
 }
