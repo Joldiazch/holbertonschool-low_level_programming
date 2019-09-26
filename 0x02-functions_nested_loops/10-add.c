9ace4fde65d2ccef0c51993b2340686c25318f2a
joldiazch <jluis.diaz@udea.edu.co>
2019-09-26 04:35:42 +0000

    punto 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+nclude "holberton.h"
+/**
+  * add - adds to integers
+  * @a: first integer parameter to be added
+  * @b: second integer parameter to be added
+  * Return: the result of the add of a and b
+  */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
