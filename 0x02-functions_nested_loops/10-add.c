a37427af5e0b9f1f128ba1c77fad896f197bf7a1
joldiazch <jluis.diaz@udea.edu.co>
2019-09-26 04:38:20 +0000

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
-nclude "holberton.h"
+#include "holberton.h"
 /**
-  * add - adds to integers
-  * @a: first integer parameter to be added
-  * @b: second integer parameter to be added
-  * Return: the result of the add of a and b
-  */
+* add - adds to integers
+* @a: first integer parameter to be added
+* @b: second integer parameter to be added
+* Return: the result of the add of a and b
+*/
 int add(int a, int b)
 {
 	int result;
