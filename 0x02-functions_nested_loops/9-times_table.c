8902236a61495242f6ab4f4f165281a18858c15b
joldiazch <jluis.diaz@udea.edu.co>
2019-09-26 04:24:58 +0000

    punto 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* times_table - imprime la tabla de 0 al 9
+*
+*
+* Return: void
+*
+*/
+void times_table(void)
+{
+	int i;
+	int j;
+	int r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; h <= 9; j++)
+		{
+			r = j * i;
+			_putchar('0' + r);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ' + M);
+			if (j == 9)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
