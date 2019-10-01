611966123974747a7a6cbdf1ab703d6b379f7234
joldiazch <jluis.diaz@udea.edu.co>
2019-10-01 20:25:18 +0000

    modificated 6 file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,15 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int j;
 
 	while (*(str + i) != 0)
 	{
-		_putchar(*(str + i));
-		i += 2;
+		i++;
+	}
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(*(str + j));
 	}
 	_putchar('\n');
 }
