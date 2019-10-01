155ed78da18cc10e93ae75956ef1cd63ebbcf383
joldiazch <jluis.diaz@udea.edu.co>
2019-10-01 18:20:12 +0000

    modificate 7 file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,13 +15,13 @@ void puts_half(char *str)
 	{
 		i++;
 	}
-	if (i % 2)
+	if ((i % 2) == 0)
 	{
-		c = (i - 1) / 2;
+		c = (i / 2);
 	}
 	else
 	{
-		c = i / 2;
+		c = (i / 2) + 1;
 	}
 	while (*(str + c) != 0)
 	{
