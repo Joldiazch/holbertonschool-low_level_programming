8008c841f6671c3767671867de23ceb38f26b24f
joldiazch <jluis.diaz@udea.edu.co>
2019-10-03 23:54:01 +0000

    add 4 file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* reverse_array - copies the first n characters of the *src to dest
+* @a: destino
+* @n: entrada
+* Return: void.
+*/
+void reverse_array(int *a, int n)
+{
+	int i = (n - 1), k = (n / 2);
+	int p = 0, p2 = 0, c = 0;
+	while (i >= k)
+	{
+		p = *(a + c);
+		p2 = *(a + i);
+		*(a + i) = p;
+		*(a + c) = p2;
+		i--;
+		c++;
+	}
+}
