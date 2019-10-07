5f53650679eab46e2b580dc78dbddc394ea4bb75
joldiazch <jluis.diaz@udea.edu.co>
2019-10-07 23:07:37 +0000

    add 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+* *_strstr - cuenta cuincidencias de accept en s
+* @haystack: origen
+* @needle: cadena a comparar
+* Return: char.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, cont = 0;
+	char *k;
+
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			k = haystack;
+			while (*needle)
+			{
+				if (*needle == *haystack)
+				{
+					cont++;
+				}
+				needle++;
+				haystack++;
+				i++;
+			}
+			if (cont == i)
+			{
+				return (k);
+			}
+			haystack = k;
+		}
+		haystack++;
+	}
+	return ('\0');
+}
