8533aeeb3c54e27b0d3fee4989e54f7e9c9f3554
joldiazch <jluis.diaz@udea.edu.co>
2019-10-12 22:36:29 +0000

    add 4 task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main -  program adds positive numbers.
+* @argc: The size of the argv array.
+* @argv: An array containing the program command line arguments.
+* Return: Always 0.
+*/
+int main(int argc, char **argv)
+{
+	int i, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (**(argv + i) >= 48 && **(argv + i) <= 57)
+		{
+			sum += atoi(*(argv + i));
+		}
+		else
+		{
+			printf("Error\n");
+			return (0);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
