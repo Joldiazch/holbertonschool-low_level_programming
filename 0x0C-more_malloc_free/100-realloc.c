407cde4764170cbed5bc5cb7c481477da3175644
joldiazch <jluis.diaz@udea.edu.co>
2019-10-18 01:03:06 +0000

    add 100 task file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,7 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 		return (p);
-	for (i = 0; i < old_size; i++)
+	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		*(p + i) = *(k + i);
 	}
