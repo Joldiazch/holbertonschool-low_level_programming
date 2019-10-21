5c0bfa4eb3648ce0b2c2b4e684d910e83abe4525
joldiazch <jluis.diaz@udea.edu.co>
2019-10-21 23:17:54 +0000

    add 4 task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+* *new_dog - create new dog
+* @name: name of dog
+* @age: age
+* @owner: owner of dog.
+* Return: new_dog estructur
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int l1 = 0, l2 = 0;
+	char *nombre, *dueno;
+
+	nombre = name;
+	dueno = owner;
+	dog_t *new_dog;
+
+	while (*(name + l1))
+	{
+		l1++;
+	}
+	while (*(owner + l2))
+	{
+		l2++;
+	}
+	new_dog = malloc(sizeof(float) + l1 + l2);
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	(*new_dog).name = nombre;
+	(*new_dog).age = age;
+	(*new_dog).owner = dueno;
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,7 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
