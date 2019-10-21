94bfcf3c31f6fb59dd2e803a15f488f64cdfc3f9
joldiazch <jluis.diaz@udea.edu.co>
2019-10-21 18:29:13 +0000

    modificate 3 task

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LiKE_MACRO_H
 #define FUNCTION_LiKE_MACRO_H
 
-#define ABS(a) ((a < 0) ? (a * -1) : (a = a);
+#define ABS(a) ((a < 0) ? (a * -1) : (a * 1))
 
 #endif /* FUNCTION_LiKE_MACRO_H */
