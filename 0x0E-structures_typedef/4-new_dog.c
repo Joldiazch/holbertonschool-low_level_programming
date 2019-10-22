24058af87db3f941bfd4ce45357960df5bbb910e
joldiazch <jluis.diaz@udea.edu.co>
2019-10-22 02:12:42 +0000

    add 4 task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -40,9 +40,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(new_dog);
 			return (NULL);
 		}
-		_strcpy((*new_dog).name, name);
+		(*new_dog).name = name;
 		(*new_dog).age = age;
-		_strcpy((*new_dog).owner, owner);
+		(*new_dog).owner = owner;
 	}
 	return (new_dog);
 }
