877e8c94126d86fd527142ef226e81aff330790b
joldiazch <jluis.diaz@udea.edu.co>
2019-10-22 01:11:26 +0000

    add 4 task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,27 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include "holberton.h"
-#include <stdio.h>
-
-/**
-  * *_strcpy - copies pointed src, to the pointed to by dest.
-  * @dest: pointer to paste
-  * @src: pointer to copy
-  * Return: Void.
-  *
-  */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (*(src + i) != 0)
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	*(dest + i) = *(src + i);
-	return (dest);
-}
 /**
 * *new_dog - create new dog
 * @name: name of dog
