3476ed0b837e18af98c91d3e6504ee748b92d18e
joldiazch <jluis.diaz@udea.edu.co>
2019-10-22 00:42:27 +0000

    add 4 task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,34 +12,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int l1 = 0, l2 = 0;
 	dog_t *new_dog;
 
-	while (*(name + l1))
+	if (name && owner)
 	{
-		l1++;
+		while (*(name + l1))
+		{
+			l1++;
+		}
+		while (*(owner + l2))
+		{
+			l2++;
+		}
+		new_dog = malloc(sizeof(dog_t));
+		if (new_dog == NULL)
+		{
+			return (NULL);
+		}
+		(*new_dog).name = malloc(l1);
+		if ((*new_dog).name == NULL)
+		{
+			free(new_dog);
+			return (NULL);
+		}
+		(*new_dog).owner = malloc(l2);
+		if ((*new_dog).owner == NULL)
+		{
+			free((*new_dog).name);
+			free(new_dog);
+			return (NULL);
+		}
+		(*new_dog).name = name;
+		(*new_dog).age = age;
+		(*new_dog).owner = owner;
 	}
-	while (*(owner + l2))
-	{
-		l2++;
-	}
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-	{
-		return (NULL);
-	}
-	(*new_dog).name = malloc(l1);
-	if ((*new_dog).name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	(*new_dog).owner = malloc(l2);
-	if ((*new_dog).owner == NULL)
-	{
-		free((*new_dog).name);
-		free(new_dog);
-		return (NULL);
-	}
-	(*new_dog).name = name;
-	(*new_dog).age = age;
-	(*new_dog).owner = owner;
 	return (new_dog);
 }
