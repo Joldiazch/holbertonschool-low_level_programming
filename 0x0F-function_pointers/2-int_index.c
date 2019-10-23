fc9cf8573ee9053835fab4964c34af5bf6f8d811
Holberton <holberton@Holbertons-iMac-45.local>
2019-10-23 15:57:25 -0500

    add 2 task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,6 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	if (cmp == NULL)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
