8173a8bbce2ca577f30e0bb0313602c9ee3211f6
joldiazch <jluis.diaz@udea.edu.co>
2019-10-28 04:33:18 +0000

    modificate 0 task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,19 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list num;
-	unsigned int i;
-	int resul = 0;
+	if (n)
+	{
+		va_list num;
+		unsigned int i;
+		int resul = 0;
 
-	if (n == 0)
-	{
-		return (0);
+		va_start(num, n);
+		for (i = 0; i < n; i++)
+		{
+			resul += va_arg(num, int);
+		}
+		va_end(num);
+		return (resul);
 	}
-	va_start(num, n);
-	for (i = 0; i < n; i++)
-	{
-		resul += va_arg(num, int);
-	}
-	va_end(num);
-	return (resul);
+	return (0);
 }
