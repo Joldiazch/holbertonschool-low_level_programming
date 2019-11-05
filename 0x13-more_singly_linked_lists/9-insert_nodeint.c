1228b9b55131089dfeab5bb4bdf9fe7aab4e12f1
joldiazch <jluis.diaz@udea.edu.co>
2019-11-05 20:17:35 +0000

    add case index 0 and len of list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,49 @@
 #include "lists.h"
+
+/**
+* add_nodeint - print all elements of a list staring in head.
+* @head: pointer for head of lements.
+* @n: integer of new node.
+* Return: pointer to new node.
+*
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = malloc(sizeof(listint_t));
+	if (!temp) /* if temp == NULL */
+	{
+		return (NULL);
+	}
+	temp->n = n;
+	temp->next = (*head);
+	/* To the "previus" head, becouse temp will be the new head */
+	(*head) = temp;
+	/* new head is temp */
+	return (temp);
+}
+/**
+* listint_len - print all elements of a list staring in head.
+* @h: pointer for head of lements.
+* Return: number of nods in list.
+*
+*/
+size_t listint_len(const listint_t *h)
+{
+	unsigned int numNodos = 0;
+
+	if (h)
+	{
+		while (h->next)
+		{
+			h = h->next;
+			numNodos++;
+		}
+		return (numNodos + 1);
+	}
+	return (0);
+}
 /**
 * get_nodeint_at_index - print all elements of a list staring in head.
 * @head: pointer for head of lements.
@@ -35,9 +80,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head && (*head))
 	{
-		if (n == 0)
+		if (idx == 0)
 		{
-			return (NULL);
+			return (add_nodeint(head, n));
+		}
+		else if (idx == (listint_len(*head) - 1))
+		{
+			return (add_nodeint_end(head, n));
 		}
 		node = get_nodeint_at_index((*head), idx);
 		newnode = malloc(sizeof(listint_t));
