119f962f8997e2e58b9e03e268d065ae1df94150
joldiazch <jluis.diaz@udea.edu.co>
2019-11-05 14:44:25 +0000

    betty corrections

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
 * add_nodeint - print all elements of a list staring in head.
-* @h: pointer for head of lements.
-* Return: number of nods in list.
+* @head: pointer for head of lements.
+* @n: integer of new node.
+* Return: pointer to new node.
 *
 */
 listint_t *add_nodeint(listint_t **head, const int n)
@@ -12,10 +13,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	temp = malloc(sizeof(listint_t));
 	if (!temp) /* if temp == NULL */
 	{
-		return(NULL);
+		return (NULL);
 	}
 	temp->n = n;
-	temp->next = (*head); /* To the "previus" head, becouse temp will be the new head */
-	(*head) = temp; /* new head is temp */
-	return(temp);
+	temp->next = (*head);
+	/* To the "previus" head, becouse temp will be the new head */
+	(*head) = temp;
+	/* new head is temp */
+	return (temp);
 }
