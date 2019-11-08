60b9966a215805cd1383a0c76bb0dce5a41ebf5b
Holberton <holberton@Holbertons-iMac-45.local>
2019-11-07 19:27:27 -0500

    compile and return all cases

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* get_bit - print bit in the positioin index.
+* @n: decimal number.
+* @index: indice.
+* Return: bit in the index position.
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (n >> index)
+	{
+		n = n >> index;
+		return (n & 0x1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
