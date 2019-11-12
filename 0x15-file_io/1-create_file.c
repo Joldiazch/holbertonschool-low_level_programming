b09ae1f5dbba28ac03b0bfea1cd86a2b6031bfa5
Holberton <holberton@Holbertons-iMac-45.local>
2019-11-12 15:17:41 -0500

    compila and return done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,16 @@ int create_file(const char *filename, char *text_content)
 		{
 			return (-1);
 		}
-
+		if (!text_content)
+		{
+			text_content = "";
+		}
 		w = write(fd, text_content, strlen(text_content));
 		if (w < 0)
 		{
 			return (-1);
 		}
+		close(fd);
 		return (1);
 	}
 	return (-1);
