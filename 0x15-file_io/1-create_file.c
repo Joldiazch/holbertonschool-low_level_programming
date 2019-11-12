537b114cd15b83590407c4e6f5f598bb05dc5e16
Holberton <holberton@Holbertons-iMac-45.local>
2019-11-12 15:08:53 -0500

    compila and return done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+* create_file - creates a file.
+* @filename: pointer to filename string.
+* @text_content: s a NULL terminated string to write to the file
+* Return: 0 if fails or 1 in other case.
+*
+*/
+int create_file(const char *filename, char *text_content)
+{
+	if (filename)
+	{
+		int fd, w;
+
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IWUSR | S_IRUSR);
+		if (fd < 0)
+		{
+			return (-1);
+		}
+
+		w = write(fd, text_content, strlen(text_content));
+		if (w < 0)
+		{
+			return (-1);
+		}
+		return (1);
+	}
+	return (-1);
+}
