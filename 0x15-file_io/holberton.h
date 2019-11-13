a460b257e1eb7e6c3f9426b9bb2e88fa8aa6019c
Holberton <holberton@Holbertons-iMac-45.local>
2019-11-12 19:44:09 -0500

    add cp task compila and copy done

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+#include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
