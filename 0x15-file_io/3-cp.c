5c8c3580ecddb149b1c30f2e81bf8c586be61b7f
Holberton <holberton@Holbertons-iMac-45.local>
2019-11-12 20:01:49 -0500

    work while case 1024 to 1024

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,18 +1,15 @@
 #include "holberton.h"
 /**
 * main - appends text at the end of a file.
-* @arc: pointer to filename string.
+* @argc: pointer to filename string.
 * @argv: s a NULL terminated string to write to the file
 * Return: 0 if fails or 1 in other case.
 *
 */
-#ifndef BUF_SIZE
-#define BUF_SIZE 1024
-#endif
 int main(int argc, char **argv)
 {
-	int fileFrom, fileTo, openFlags, r, w;
-	char buf[BUF_SIZE];
+	int fileFrom, fileTo, openFlags, r;
+	char buf[1024];
 
 	if (argc != 3)
 	{
@@ -23,18 +20,20 @@ int main(int argc, char **argv)
 	openFlags = O_CREAT | O_WRONLY | O_TRUNC;
 	fileTo = open(argv[2],
 	openFlags, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	r = read(fileFrom, buf, BUF_SIZE);
+
+	while ((r = read(fileFrom, buf, 1024)) > 0)
+	{
+		if (write(fileTo, buf, r) != r || fileTo == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
 	if (r == -1 || fileFrom == -1)
 	{
 		dprintf(STDERR_FILENO,
 		"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}
-	w = write(fileTo, buf, r);
-	if (w == -1 || fileTo == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
 	}
 	if (close(fileTo) == -1)
 	{
