5b06750a9e38236b3bb36cd3bc67ad55222a8588
Jose Luis diaz <jluis.diaz@udea.edu.co>
2020-01-18 21:32:04 -0500

    add new malloc for hash_table_t and allocs 8,208 bytes no check betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,15 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_create -  creates a hash table.
+ * size: size of hash table.
+ * Return: pointer to hash table.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *ht = NULL;
 
-    ht = malloc(sizeof(hash_node_t *) * size);
-    return (ht == NULL ? NULL : ht);
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
+    ht->array = malloc(sizeof(hash_node_t *) * size);
+    if (ht->array == NULL)
+        return (NULL);
+    ht->size = size;
+    return (ht);
 }
