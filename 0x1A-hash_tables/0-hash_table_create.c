240c31c94bccf92d69f9a51acb0035b8bbaeedcc
Jose Luis diaz <jluis.diaz@udea.edu.co>
2020-01-19 11:24:40 -0500

    initializate all alements of ht->array in NULL and fixed betty warning

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,20 +2,23 @@
 
 /**
  * hash_table_create -  creates a hash table.
- * size: size of hash table.
+ * @size: size of hash table.
  * Return: pointer to hash table.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = NULL;
+	hash_table_t *ht = NULL;
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-        return (NULL);
-    ht->size = size;
-    return (ht);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	ht->size = size;
+	for (size_t i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }
