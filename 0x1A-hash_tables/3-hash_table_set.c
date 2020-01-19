afced0f1f97d2b365047ae1ba0c2dccc9a696eb7
joldiazch <jluis.diaz@udea.edu.co>
2020-01-19 21:21:23 +0000

    add empty case for key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -47,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
 
-	if (key == NULL || value == NULL || ht == NULL || key == "")
+	if (key == NULL || value == NULL || ht == NULL || strcmp(key, "") == 0)
 		return (0);
 
 	idx = key_index((const unsigned char *) key, ht->size);
