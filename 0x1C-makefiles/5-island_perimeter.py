7986e33adbf39537bd1a252ad0f52a407a9fb60d
joldiazch <jluis.diaz@udea.edu.co>
2020-02-21 19:41:41 -0500

    Island Perimeter Technical interview preparation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,21 +3,22 @@
 def island_perimeter(grid):
     cont = 0
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if j < len(grid[i]):
-                    if grid[i][j + 1] == 1:
-                        cont += 1
-                if j - 1 >= 0:
-                    if grid[i][j - 1] == 1:
-                        cont += 1
-                if i + 1 <= len(grid) - 1:
-                    if grid[i + 1][j] == 1:
-                        cont += 1
-                if i - 1 >= 0:
-                    if grid[i - 1][j] == 1:
-                        cont += 1
-                perimeter += 4 - cont
-                cont = 0
-    return perimeter
+    if len(grid) <= 100:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    if j < len(grid[i]):
+                        if grid[i][j + 1] == 1:
+                            cont += 1
+                    if j - 1 >= 0:
+                        if grid[i][j - 1] == 1:
+                            cont += 1
+                    if i + 1 <= len(grid) - 1:
+                        if grid[i + 1][j] == 1:
+                            cont += 1
+                    if i - 1 >= 0:
+                        if grid[i - 1][j] == 1:
+                            cont += 1
+                    perimeter += 4 - cont
+                    cont = 0
+        return perimeter
