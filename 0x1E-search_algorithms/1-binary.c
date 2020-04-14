62beda50c15b4cfa67faa94667c8175fb1fc103c
joldiazch <jluis.diaz@udea.edu.co>
2020-04-14 21:01:22 +0000

    add binary search function

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+* search - function that searches for a value in an array
+* @array: array of integer
+* @start: start range for search
+* @end: end range for search
+* @value: value to serch
+* Return: index of match or -1 other case
+*/
+int search(int *array, size_t start, size_t end, int value)
+{
+	size_t half = (end - start) / 2, i;
+
+	printf("Searching in array: ");
+	for (i = start; i < end; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[end]);
+
+	if (!array || end - start < 1)
+		return (-1);
+
+	if (value == array[start + half])
+		return (start + half);
+	else if (value > array[start + half])
+		return (search(array, start + half + 1, end, value));
+	else
+		return (search(array, start, half - 1, value));
+}
+
+/**
+* binary_search - function that searches for a value in an array
+* @array: array of integer
+* @size: size of array of integer
+* @value: value to serch
+* Return: index of match or -1 other case
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	return (search(array, 0, size - 1, value));
+}
+
+
