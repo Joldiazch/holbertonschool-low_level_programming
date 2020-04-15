8737f0baf898ff1cac00449d6bdb75512fbee6c4
joldiazch <jluis.diaz@udea.edu.co>
2020-04-15 17:53:24 +0000

    modificate libraries

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
 * jump_search - function that searches for a value in an array
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,6 @@
 #define search_algos_h
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
